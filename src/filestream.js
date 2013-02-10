5b6f06b6c92c90e13a03defca6e44a4670f5498d
Aaron Ackerman <theron17@gmail.com>
2013-02-10 12:47:09 -0600

    Added promises, removed async false on xhr open call, RTFM and don't blindly copy others code

--- src/filestream.js
+++ src/filestream.js
@@ -1,44 +1,73 @@
 ;(function(exports){
 	var ondragover = function(el){
 		el.addEventListener('dragover', function(e){
-			noop(e);
+			noDefault(e);
 			e.dataTransfer.dropEffect = 'copy';
 		});
 	};
 
-	var ondrop = function(el, url){
+	var ondrop = function(el, url, xhr){
+		var files;
 		el.addEventListener('drop', function(e){
-			noop(e);
-			var files = e.dataTransfer.files || e.target.files;
+			noDefault(e);
+			files = e.dataTransfer.files || e.target.files;
 			if (files.length) {
 				files = [].slice.call(files);
 				files.forEach(function(file){
-					stream(file, url);
+					send(file, url, xhr);
 				});
 			}
 		});
 	};
 
-	var stream = function(file, url) {
-		var fd = new FormData();
-		var xhr = new XMLHttpRequest();
-		fd.append('file', file, file.name);
-		fd.append('size', file.size);
-		xhr.open('POST', url, false);
-		xhr.send(fd);
+	var Promise = function(xhr){
+		this.xhr = xhr;
+	};
+
+	Promise.prototype = {
+		then: function(callback) {
+			var xhr = this.xhr;
+			xhr.onreadystatechange = function(e){
+				if(e.target.readyState == 4) {
+					callback(xhr.response);
+				}
+			};
+			return this;
+		},
+		progress: function(callback) {
+			this.xhr.upload.onprogress = function(e){
+				if(e.lengthComputable) {
+					callback((e.loaded / e.total) * 100);
+				}
+			};
+			return this;
+		}
 	};
 
 	// base function
 	var filestream = function(attrs) {
+		var xhr = new XMLHttpRequest();
+		var promise = new Promise(xhr);
 		ondragover(attrs.el);
-		ondrop(attrs.el, attrs.url);
+		ondrop(attrs.el, attrs.url, xhr);
+		return promise;
+	};
+
+	var send = function(file, url, xhr) {
+		var fd = new FormData();
+		fd.append('file', file, file.name);
+		fd.append('size', file.size);
+		xhr.open('POST', url);
+		xhr.send(fd);
 	};
 
 	// prevent default form actions
-	var noop = function(e) {
+	var noDefault = function(e) {
 		e.preventDefault();
 		e.stopPropagation();
 	};
+
+	var noop = function(){};
 
 	// expose filestream to the window and as an AMD module
 	if (typeof define == 'function' && typeof define.amd == 'object' && define.amd) {
